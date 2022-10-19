0215c1dfd6ed097b16ca96b54cacf6e30fb9492b
owni14 <kmwoo514@gmail.com>
2022-10-19 18:23:45 +0900

    finished radio box filter

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -7,6 +7,7 @@ import ImageSlider from '../../utils/ImageSlider';
 import CheckBox from './Sections/CheckBox';
 import { continents, price } from './Sections/Datas';
 import RadioBox from './Sections/RadioBox';
+import SearchFeature from './Sections/SearchFeature';
 
 function LandingPage() {
   const [Products, setProducts] = useState([]);
@@ -17,6 +18,7 @@ function LandingPage() {
     continents: [],
     price: [],
   });
+  const [SearchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     let body = {
@@ -101,6 +103,19 @@ function LandingPage() {
     setFilters(newFilters);
   };
 
+  const updateSearchTerm = (newSearchTerm) => {
+    let body = {
+      skip: 0,
+      limit: Limit,
+      filters: Filters,
+      searchTerm: newSearchTerm,
+    };
+
+    setSkip(0);
+    setSearchTerm(newSearchTerm);
+    getProducts(body);
+  };
+
   return (
     <div style={{ width: '75%', margin: '3rem auto' }}>
       <div style={{ textAlign: 'center' }}>
@@ -129,6 +144,15 @@ function LandingPage() {
       </Row>
 
       {/* Search */}
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          margin: '1rem auto',
+        }}
+      >
+        <SearchFeature refreshFunction={updateSearchTerm} />
+      </div>
 
       {/* Cards */}
 
--- server/models/Product.js
+++ server/models/Product.js
@@ -39,6 +39,18 @@ const productSchema = mongoose.Schema(
   { timestamps: true }
 );
 
+productSchema.index(
+  {
+    title: 'text',
+    description: 'text',
+  },
+  {
+    wweights: {
+      title: 5,
+      description: 1,
+    },
+  }
+);
 const Product = mongoose.model('Product', productSchema);
 
 module.exports = { Product };
