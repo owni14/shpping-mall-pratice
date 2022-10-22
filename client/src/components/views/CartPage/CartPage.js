f92e3e5ae531675b6d214ce209a110bb293b5b53
owni14 <kmwoo514@gmail.com>
2022-10-22 11:34:07 +0900

    added paypal button

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { getCartItems, removeCartItem } from '../../../_actions/user_actions';
 import UserCardBlock from './Sections/UserCardBlock';
 import { Empty } from 'antd';
+import Paypal from '../../utils/Paypal';
 
 function CartPage(props) {
   const dispatch = useDispatch();
@@ -66,6 +67,8 @@ function CartPage(props) {
           <Empty description={false} />
         </>
       )}
+
+      <Paypal />
     </div>
   );
 }
